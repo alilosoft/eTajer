f484d3fd795de0c36f7d8d4fd358e1daa07a4381
@lilo$oft <fellahi.ali@gmail.com>
2020-07-27 23:31:55 +0100

    remove create Product with id test
    this logic is an implementation detail that should be moved to an implementation of Product abstraction

--- src/test/kotlin/etajer/cashier/objects/ProductAssumptions.kt
+++ src/test/kotlin/etajer/cashier/objects/ProductAssumptions.kt
@@ -4,12 +4,6 @@ import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 
 class ProductAssumptions {
-    @Test
-    fun `create a Product with it's id`() {
-        val id = 100
-        val p = Product(id)
-        assertNotNull(p, "we should create a Product with an id")
-    }
 
     @Test
     fun `a Product should have a readonly id property`() {
