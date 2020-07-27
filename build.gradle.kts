81d98c26e2519f21956a04a5f8dc18de6a42710a
@lilo$oft <fellahi.ali@gmail.com>
2020-07-27 19:21:54 +0100

    add dependencies: junit 5.6.2 and logback-classic

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,13 @@ repositories {
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom")) // Align versions of all Kotlin components
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    // logging
+    implementation("ch.qos.logback:logback-classic:1.2.3")
+    // test
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
+    //testImplementation("org.jetbrains.kotlin:kotlin-test")
+    //testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 }
 
 application {
