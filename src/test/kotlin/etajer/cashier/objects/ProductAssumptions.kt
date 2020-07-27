ca4e7fd162af7e6cdb9f1228f14ab2abf2db3b2e
@lilo$oft <fellahi.ali@gmail.com>
2020-07-27 17:54:02 +0100

    rename ProductTest to ProductAssumptions

--- src/test/kotlin/etajer/cashier/objects/ProductAssumptions.kt
+++ src/test/kotlin/etajer/cashier/objects/ProductAssumptions.kt
@@ -3,12 +3,12 @@ package etajer.cashier.objects
 import org.junit.Test
 import kotlin.test.assertNotNull
 
-class ProductTest {
+class ProductAssumptions {
     @Test
     fun `create a Product with it's id`() {
         val id = 100
         val p = Product(id)
-        assertNotNull(p,"we should create a Product with an id")
+        assertNotNull(p, "we should create a Product with an id")
     }
 
 }
