c39f3a6d268ae725017b50cfbc5dcf6d4d6035e1
@lilo$oft <fellahi.ali@gmail.com>
2020-08-05 15:22:38 +0100

    add DerbyDb integration tests

---
+++ src/test/kotlin/etajer/cashier/jdbc/DerbyDbTests.kt
@@ -0,0 +1,30 @@
+package etajer.cashier.jdbc
+
+import com.vladsch.kotlin.jdbc.session
+import com.vladsch.kotlin.jdbc.using
+import com.zaxxer.hikari.HikariDataSource
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class DerbyDbTests {
+    @Test
+    fun `should be using HikariCP datasource`() {
+        // Arrange
+        // Act
+        val ds = DerbyDb.dataSource
+        // Assert
+        assertTrue(ds is HikariDataSource)
+    }
+
+    @Test
+    fun `using() should close the connection but not the datasource`() {
+        // Arrange
+        val ds = DerbyDb.dataSource
+        // Act
+        val conn = using(session(ds)) { s -> s.connection }
+        // Assert
+        assertTrue(conn.isClosed)
+        assertFalse(ds.connection.isClosed)
+    }
+}
