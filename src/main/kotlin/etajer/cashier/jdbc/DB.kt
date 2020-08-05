5ef99b2adf1426315960a8443d1b14549d5a32d4
@lilo$oft <fellahi.ali@gmail.com>
2020-08-05 14:00:34 +0100

    introduce DB interface with dataSource property

--- src/main/kotlin/etajer/cashier/jdbc/DB.kt
+++ src/main/kotlin/etajer/cashier/jdbc/DB.kt
@@ -1,9 +1,14 @@
 package etajer.cashier.jdbc
 
 import com.vladsch.kotlin.jdbc.HikariCP
+import javax.sql.DataSource
 
-object DerbyDB {
+interface DB {
+    val dataSource: DataSource
+}
+
+object DerbyDB : DB {
     // TODO: load config from file
     private const val url = "jdbc:derby://localhost:1527/dine2020"
-    val dataSource = HikariCP.default(url, "alilo", "alilo")
+    override val dataSource : DataSource = HikariCP.default(url, "alilo", "alilo")
 }
