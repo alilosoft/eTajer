cea97b59d8b082aef4114903622393c157164cdd
alilosoft <fellahi.ali@gmail.com>
2020-09-03 23:54:30 +0100

    Products.add() accept a name and price parameters

--- src/main/kotlin/etajer/cashier/objects/Products.kt
+++ src/main/kotlin/etajer/cashier/objects/Products.kt
@@ -4,7 +4,7 @@ import com.gojuno.koptional.Optional
 
 
 interface Products {
-    fun add(new: Product): Product
+    fun add(name: String, price: Double): Product
     /**
      * Assuming that a Product should exist with the given `id`, this method will return an instance of Product.
      * Otherwise any implementation should throw an IllegalStateException
