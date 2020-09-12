c9fa64c774db98f81589c929cbd690764506c32e
alilosoft <fellahi.ali@gmail.com>
2020-09-12 14:52:07 +0100

    #8 update java doc for SaleUnit interface

--- src/main/kotlin/etajer/cashier/objects/SaleUnit.kt
+++ src/main/kotlin/etajer/cashier/objects/SaleUnit.kt
@@ -3,5 +3,10 @@ package etajer.cashier.objects
 interface SaleUnit {
     val name: String
     val qty: Int
+    /**
+     * The implementations of SaleUnit should decide/know how to set the price.
+     * It could be fixed unit price, or calculated based on the Product price
+     * using the formula S
+     */
     val price: Double
 }
