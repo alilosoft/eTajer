147181a77976c00410dc4106ed67ff4dadcfe1f7
alilosoft <fellahi.ali@gmail.com>
2020-09-12 15:04:46 +0100

    #10 update java doc for SaleUnit interface

--- src/main/kotlin/etajer/cashier/objects/SaleUnit.kt
+++ src/main/kotlin/etajer/cashier/objects/SaleUnit.kt
@@ -2,6 +2,11 @@ package etajer.cashier.objects
 
 interface SaleUnit {
     val name: String
+    /**
+     * todo: according to issue #10, maybe this property should be removed!
+     * The number of units packed by this sale unit if it's a package,
+     * if it's a Unitary unit this should be 1
+     */
     val qty: Int
     /**
      * The implementations of SaleUnit should decide/know how to set the price.
