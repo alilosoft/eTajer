fbec9009c7517fe9cd891c34dd0b181ff1538bc5
alilosoft <fellahi.ali@gmail.com>
2020-09-13 11:56:14 +0100

    #10 remove Qty prop from SaleUnit interface

--- src/main/kotlin/etajer/cashier/objects/SaleUnit.kt
+++ src/main/kotlin/etajer/cashier/objects/SaleUnit.kt
@@ -3,12 +3,6 @@ package etajer.cashier.objects
 interface SaleUnit {
     val name: String
     /**
-     * todo: according to issue #10, maybe this property should be removed!
-     * The number of units packed by this sale unit if it's a package,
-     * if it's a Unitary unit this should be 1
-     */
-    val qty: Int
-    /**
      * The implementations of SaleUnit should decide/know how to set the price.
      * It could be fixed unit price, or calculated based on the Product price
      * using the formula S
