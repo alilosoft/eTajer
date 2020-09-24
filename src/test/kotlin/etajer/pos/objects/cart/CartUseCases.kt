ec4862394a5664feb34c6931a0020efd4ce90f09
alilosoft <fellahi.ali@gmail.com>
2020-09-24 10:35:51 +0100

    #17 Cart public iterator() is immutable now :)

--- src/test/kotlin/etajer/pos/objects/cart/CartUseCases.kt
+++ src/test/kotlin/etajer/pos/objects/cart/CartUseCases.kt
@@ -3,20 +3,23 @@ package etajer.pos.objects.cart
 import etajer.pos.data.fake.FakeSKUs
 import etajer.pos.objects.CartItem
 import etajer.pos.objects.createCartItemBySku
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 import java.time.LocalTime
 
-interface Cart : MutableIterable<CartItem> {
+interface Cart : Iterable<CartItem> {
     val number: Int
     val date: LocalDate
     val time: LocalTime
 
+    // TODO: return a boolean for add and remove
     fun addItem(item: CartItem)
+    fun removeItem(item: CartItem)
 }
 
-val fakeSimpleCart = object : Cart {
+val fakeCart = object : Cart {
     override val number: Int = 1
     override val date: LocalDate = LocalDate.now()
     override val time: LocalTime = LocalTime.now()
@@ -26,7 +29,11 @@ val fakeSimpleCart = object : Cart {
         items.add(item)
     }
 
-    override fun iterator(): MutableIterator<CartItem> = items.iterator()
+    override fun removeItem(item: CartItem) {
+        items.remove(item)
+    }
+
+    override fun iterator(): Iterator<CartItem> = items.iterator()
 
     override fun toString(): String {
 
@@ -43,9 +50,21 @@ class CartUseCases {
         // Arrange
         val facto = createCartItemBySku(FakeSKUs.FACTO, 2)
         // Act
-        fakeSimpleCart.addItem(facto)
-        println(fakeSimpleCart)
+        fakeCart.addItem(facto)
+        println(fakeCart)
         // Assert
-        assertEquals(1, fakeSimpleCart.toList().size)
+        assertTrue(fakeCart.iterator().hasNext())
+    }
+
+    @Test
+    fun `remove an (existing) item from th Cart`() {
+        // Arrange
+        val facto = createCartItemBySku(FakeSKUs.FACTO, 2)
+        // Act
+        fakeCart.addItem(facto)
+        assertTrue(fakeCart.iterator().hasNext())
+        fakeCart.removeItem(facto)
+        // Assert
+        assertFalse(fakeCart.iterator().hasNext())
     }
 }
