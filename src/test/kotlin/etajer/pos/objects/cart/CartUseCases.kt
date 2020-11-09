3ed3fdf4b0016d813b197d0eaf2f3d5c0055bd76
alilosoft <fellahi.ali@gmail.com>
2020-11-09 10:47:18 +0100

    #20 fix comment line

--- src/test/kotlin/etajer/pos/objects/cart/CartUseCases.kt
+++ src/test/kotlin/etajer/pos/objects/cart/CartUseCases.kt
@@ -81,9 +81,9 @@ interface Cart : Iterable<CartItem> {
     val date: LocalDate
     val time: LocalTime
 
-    // TODO: return a boolean for add and remove
     /** add an item to the Cart by its SKU */
     fun addBySku(sku: String, qty: Int = 1): CartItem
+    // TODO: return a boolean for add and remove
     fun addItem(item: CartItem) // TODO: remove
     fun removeItem(item: CartItem)
     fun total() = sumByDouble { it.total() }
